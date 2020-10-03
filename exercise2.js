c960cb2c92b0db9a58a4bf29a0643b6a3be143fe
Xia Jing <1251816747@qq.com>
2020-10-03 20:47:29 +0100

    Exercise 2

--- exercise2.js
+++ exercise2.js
@@ -1,11 +1,9 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(json => {
-     const completed = json.filter((todo) => todo.completed)
-     completed.forEach( (todo, index) => {
-      console.log(`${todo.title} - ${todo.completed}`)
+     const todoTitles = json.map((todo) => todo.title)
+        console.log(todoTitles)
     })
-  })
   .catch(function(err) { 
     console.log(err);
   });
